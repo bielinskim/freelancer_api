d1a68aa589225e6964defd32a594e4572484d71e
bielinskim <bielinskiim@gmail.com>
2020-06-07 13:19:04 +0200

    fix

--- server.js
+++ server.js
@@ -180,7 +180,7 @@ app.post("/register", function (req, res) {
         req.body.login,
         function (err, user) {
             if (err) throw err;
-            row.user = user;
+            var user = user;
             if (user.length === 0) {
                 con.query(
                     "INSERT INTO users(login, password, email, role_id) VALUES ('" +
